5fdb8daee2aeabfd0108a32a634ba418a3f0a75d
Stroe Stefan <stroestefan916@gmail.com>
2021-10-27 08:53:00 +0300

    Laborator 5

--- Tests/testCRUD.py
+++ Tests/testCRUD.py
@@ -13,6 +13,17 @@ def testAdaugaLibrarie():
     assert getPret(getById("1", lista)) == 15
     assert getReducere(getById("1", lista)) == "none"
 
+
+    lista = []
+    lista = adaugaLibrarie("2", "Ion", "Realism", 20, "silver", lista)
+
+    assert len(lista) == 1
+    assert getId(getById("2", lista)) == "2"
+    assert getTitlu(getById("2", lista)) == "Ion"
+    assert getGen(getById("2", lista)) == "Realism"
+    assert getPret(getById("2", lista)) == 20
+    assert getReducere(getById("2", lista)) == "silver"
+
 def testStergeLibrarie():
     lista = []
     lista = adaugaLibrarie("1", "Baltagul", "Traditionalism", 15, "none", lista)
@@ -28,6 +39,19 @@ def testStergeLibrarie():
 
     assert len(lista) == 1
     assert getById("2", lista) is not None
+
+
+    lista = []
+    lista = adaugaLibrarie("1", "Baltagul", "Traditionalism", 15, "none", lista)
+    lista = adaugaLibrarie("2", "Ion", "Realism", 20, "silver", lista)
+    lista = adaugaLibrarie("3", "Enigma Otiliei", "Realism", 25, "gold", lista)
+
+    lista = stergeLibrarie("2", lista)
+
+    assert len(lista) == 2
+    assert getById("1", lista) is not None
+    assert getById("2", lista) is None
+    assert getById("3", lista) is not None
 
 def testModificaLibrarie():
     lista = []
@@ -61,3 +85,24 @@ def testModificaLibrarie():
     assert getGen(librariaNeupdatata) == "Traditionalism"
     assert getPret(librariaNeupdatata) == 15
     assert getReducere(librariaNeupdatata) == "none"
+
+
+    lista = []
+    lista = adaugaLibrarie("1", "Baltagul", "Traditionalism", 15, "none", lista)
+    lista = adaugaLibrarie("2", "Ion", "Realism", 20, "silver", lista)
+
+    lista = modificaLibrarie("2", "Enigma Otiliei", "Realism", 5, "gold", lista)
+
+    librariaNeupdatata = getById("1", lista)
+    assert getId(librariaNeupdatata) == "1"
+    assert getTitlu(librariaNeupdatata) == "Baltagul"
+    assert getGen(librariaNeupdatata) == "Traditionalism"
+    assert getPret(librariaNeupdatata) == 15
+    assert getReducere(librariaNeupdatata) == "none"
+
+    librariaUpdatata = getById("2", lista)
+    assert getId(librariaUpdatata) == "2"
+    assert getTitlu(librariaUpdatata) == "Enigma Otiliei"
+    assert getGen(librariaUpdatata) == "Realism"
+    assert getPret(librariaUpdatata) == 5
+    assert getReducere(librariaUpdatata) == "gold"
--- Tests/testDomain.py
+++ Tests/testDomain.py
@@ -9,3 +9,21 @@ def testLibrarie():
     assert getGen(librarie) == "Traditionalism"
     assert getPret(librarie) == 15
     assert getReducere(librarie) == "none"
+
+
+    librarie2 = creeazaLibrarie("2", "Ion", "Realism", 20, "silver")
+
+    assert getId(librarie2) == "2"
+    assert getTitlu(librarie2) == "Ion"
+    assert getGen(librarie2) == "Realism"
+    assert getPret(librarie2) == 20
+    assert getReducere(librarie2) == "silver"
+
+
+    librarie3 = creeazaLibrarie("3", "Enigma Otiliei", "Realism", 25, "gold")
+
+    assert getId(librarie3) == "3"
+    assert getTitlu(librarie3) == "Enigma Otiliei"
+    assert getGen(librarie3) == "Realism"
+    assert getPret(librarie3) == 25
+    assert getReducere(librarie3) == "gold"
